08b2e1cbd3b0e9fb578ef7caa258fa3d9196d8ae
AbhilakshSinghReen <abhilakshreen@gmail.com>
2024-10-23 19:07:49 +0530

    first commit

---
+++ _next/static/hQwECBHV9o0WmX2S0pG39/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[categorySlug]","\u002F[categorySlug]\u002F[postOrSeriesSlug]","\u002F[categorySlug]\u002F[postOrSeriesSlug]\u002F[seriesPartNumberSlug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
